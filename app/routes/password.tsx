5b3a86198110687c6d7a6c3359d00d7be8ceda93
Space Cowboy <dxb@boardmansgame.com>
2026-01-27 03:00:06 +0000

    Modify password page.

--- app/routes/password.tsx
+++ app/routes/password.tsx
@@ -182,7 +182,7 @@ export default function PasswordPage() {
           {!isLoggedIn && (
             <>
               <p className="text-green-700 font-bold text-center text-sm">
-                Enter password below '{storePassword}':
+                Enter password below:
               </p>
 
               <div className="relative">
